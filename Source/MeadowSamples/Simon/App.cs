85ae1e57491a670ff87a493f9e1e7949971aff22
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-09-17 00:58:29 -0700

    [Simon] Initial check-in

---
+++ Source/MeadowSamples/Simon/App.cs
@@ -0,0 +1,44 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Leds;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Simon
+{
+    public class App : App<F7Micro, App>
+    {
+        Led ledRed;
+        Led ledGreen;
+        Led ledBlue;
+        Led ledYellow;
+
+        public App()
+        {
+            ledRed    = new Led(Device.CreateDigitalOutputPort(Device.Pins.D10));
+            ledGreen  = new Led(Device.CreateDigitalOutputPort(Device.Pins.D09));
+            ledBlue   = new Led(Device.CreateDigitalOutputPort(Device.Pins.D08));
+            ledYellow = new Led(Device.CreateDigitalOutputPort(Device.Pins.D07));
+
+            TestLEDs();
+        }
+
+        protected void TestLEDs()
+        {
+            bool state = true;
+
+            while(true)
+            {
+                ledRed.IsOn = state;
+                ledGreen.IsOn = state;
+                ledBlue.IsOn = state;
+                ledYellow.IsOn = state;
+
+                state = !state;
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Source/MeadowSamples/Simon/Program.cs
@@ -0,0 +1,16 @@
+﻿using Meadow;
+using System.Threading;
+
+namespace Simon
+{
+    class Program
+    {
+        static IApp app;
+
+        static void Main(string[] args)
+        {
+            app = new App();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
