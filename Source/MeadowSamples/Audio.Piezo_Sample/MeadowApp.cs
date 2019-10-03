ecbe22d740e53a21ae12fdf8d02b3166e759822a
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-10-02 23:22:21 -0700

    Add Audio.Piezo_Sample project

---
+++ Source/MeadowSamples/Audio.Piezo_Sample/MeadowApp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation.Audio;
+
+namespace Audio.Piezo_Sample
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        readonly PiezoSpeaker piezoSpeaker;
+
+        public MeadowApp()
+        {
+            piezoSpeaker = new PiezoSpeaker(Device.CreatePwmPort(Device.Pins.D05));
+
+            TestPiezoSpeaker();
+        }
+
+        protected void TestPiezoSpeaker()
+        {
+            while (true)
+            {
+                Console.WriteLine("Playing A4 note!");
+                piezoSpeaker.PlayTone(440, 1000);
+                piezoSpeaker.StopTone();
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
