27c30721f18f916542e55760857524873dbb3b88
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-10-05 00:43:21 -0700

    Add MCP23008_Sample Project

---
+++ Source/MeadowSamples/Samples/ICs.IOExpanders.MCP23008_Sample/MCP23008PinDefinitions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICs.IOExpanders.MCP23008_Sample
+{
+    public class MCP23008PinDefinitions
+    {
+    }
+}
