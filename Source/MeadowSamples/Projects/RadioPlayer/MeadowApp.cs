04a100ed439e99295214bd45d4b467d61dd7afb5
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-11-22 22:59:06 -0800

    [RadioPlayer] Finished project V1 with a list of known stations (scanning needs some work)

--- Source/MeadowSamples/Projects/RadioPlayer/MeadowApp.cs
+++ Source/MeadowSamples/Projects/RadioPlayer/MeadowApp.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using Meadow;
 using Meadow.Devices;
+using Meadow.Foundation.Audio.Radio;
 using Meadow.Foundation.Displays;
 using Meadow.Foundation.Graphics;
 using Meadow.Foundation.Sensors.Buttons;
@@ -11,24 +13,42 @@ namespace RadioPlayer
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
-        float currentFrequency;
+        List<float> stations;
+        int currentStation = 0;
 
-        PushButton btnNext;
-        PushButton btnPrevious;
+        TEA5767 radio;
         SSD1306 display;
         GraphicsLibrary graphics;
+        PushButton btnNext;
+        PushButton btnPrevious;
 
         public MeadowApp()
         {
             InitializePeripherals();
-            Start();
+
+            stations = new List<float>();
+            stations.Add(94.5f);
+            stations.Add(95.3f);
+            stations.Add(96.9f);
+            stations.Add(102.7f);
+            stations.Add(103.5f);
+            stations.Add(104.3f);
+            stations.Add(105.7f);
+
+            DisplayText("Radio Player");
+            Thread.Sleep(1000);
+            radio.SelectFrequency(stations[currentStation]);
+            DisplayText($"<- FM {stations[currentStation]} ->");
         }
 
-        public void InitializePeripherals()
+        void InitializePeripherals()
         {
             Console.WriteLine("Creating Outputs...");
 
             var i2CBus = Device.CreateI2cBus();
+
+            radio = new TEA5767(i2CBus);
+
             display = new SSD1306(i2CBus, 60, SSD1306.DisplayType.OLED128x32);
             graphics = new GraphicsLibrary(display);
             graphics.Rotation = GraphicsLibrary.RotationType._180Degrees;
@@ -38,33 +58,28 @@ namespace RadioPlayer
 
             btnPrevious = new PushButton(Device.CreateDigitalInputPort(Device.Pins.D04, InterruptMode.EdgeBoth, ResistorMode.Disabled));
             btnPrevious.Clicked += BtnPreviousClicked;
-
-            currentFrequency = 94.5f;
         }
 
         void BtnNextClicked(object sender, EventArgs e)
         {
-            DisplayText("      >>>>      ", 0);
-            Thread.Sleep(1000);
-            currentFrequency++;
-            DisplayText($"<- FM {currentFrequency.ToString()} ->");
+            if (currentStation < stations.Count-1)
+            {
+                DisplayText("      >>>>      ", 0);
+                currentStation++;
+                radio.SelectFrequency(stations[currentStation]);                
+                DisplayText($"<- FM {stations[currentStation]} ->");
+            }
         }
 
         void BtnPreviousClicked(object sender, EventArgs e)
         {
-            DisplayText("      <<<<      ", 0);
-            Thread.Sleep(1000);
-            currentFrequency--;
-            DisplayText($"<- FM {currentFrequency.ToString()} ->");
-        }
-
-        void Start()
-        {
-            Console.WriteLine("Start...");
-
-            DisplayText("Radio Player");
-            Thread.Sleep(3000);
-            DisplayText($"<- FM {currentFrequency.ToString()} ->");
+            if (currentStation > 0)
+            {
+                DisplayText("      <<<<      ", 0);
+                currentStation--;
+                radio.SelectFrequency(stations[currentStation]);                
+                DisplayText($"<- FM {stations[currentStation]} ->");
+            }
         }
 
         void DisplayText(string text, int x = 12)
@@ -74,6 +89,6 @@ namespace RadioPlayer
             graphics.DrawRectangle(0, 0, 128, 32);
             graphics.DrawText(x, 12, text);
             graphics.Show();
-        }
+        }       
     }
 }
