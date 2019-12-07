ef8fbef30b6bd38a9518942dd245e7655860e564
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-12-07 00:12:00 -0800

    [JinglePlayer] Add new project

---
+++ Source/MeadowSamples/Projects/JinglePlayer/MeadowApp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Hardware;
+
+namespace JinglePlayer
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        IDigitalOutputPort redLed;
+        IDigitalOutputPort blueLed;
+        IDigitalOutputPort greenLed;
+
+        public MeadowApp()
+        {
+            ConfigurePorts();
+            BlinkLeds();
+        }
+
+        public void ConfigurePorts()
+        {
+            Console.WriteLine("Creating Outputs...");
+            redLed = Device.CreateDigitalOutputPort(Device.Pins.OnboardLedRed);
+            blueLed = Device.CreateDigitalOutputPort(Device.Pins.OnboardLedBlue);
+            greenLed = Device.CreateDigitalOutputPort(Device.Pins.OnboardLedGreen);
+        }
+
+        public void BlinkLeds()
+        {
+            var state = false;
+
+            while (true)
+            {
+                int wait = 200;
+
+                state = !state;
+
+                Console.WriteLine($"State: {state}");
+
+                redLed.State = state;
+                Thread.Sleep(wait);
+                blueLed.State = state;
+                Thread.Sleep(wait);
+                greenLed.State = state;
+                Thread.Sleep(wait);
+            }
+        }
+    }
+}
