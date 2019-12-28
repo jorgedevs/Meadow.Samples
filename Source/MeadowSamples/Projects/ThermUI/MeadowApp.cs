47e12825abf5bfa3fe9e3d0742e8caef871076da
Jorge Ramirez <ramirez.jorgea@outlook.com>
2019-12-27 23:12:13 -0800

    Add ThermUI

---
+++ Source/MeadowSamples/Projects/ThermUI/MeadowApp.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+
+namespace ThermUI
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {
+        const int pulseDuration = 3000;
+        RgbPwmLed rgbPwmLed;
+
+        public MeadowApp()
+        {
+            rgbPwmLed = new RgbPwmLed(Device,
+                       Device.Pins.OnboardLedRed,
+                       Device.Pins.OnboardLedGreen,
+                       Device.Pins.OnboardLedBlue);
+
+            PulseRgbPwmLed();
+        }
+
+        protected void PulseRgbPwmLed()
+        {
+            while (true)
+            {
+                Pulse(Color.Red);
+                Pulse(Color.Green);
+                Pulse(Color.Blue);
+            }
+        }
+
+        protected void Pulse(Color color)
+        {
+            rgbPwmLed.StartPulse(color);
+            Console.WriteLine($"Pulsing {color}");
+            Thread.Sleep(pulseDuration);
+            rgbPwmLed.Stop();
+        }
+    }
+}
