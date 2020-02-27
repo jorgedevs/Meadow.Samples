28b761b4cfc25a82f1d325d406940e7fffea8520
Jorge Ramirez <ramirez.jorgea@outlook.com>
2020-02-26 22:56:28 -0800

    [ServoButton] Small update

--- Source/MeadowSamples/Projects/ServoButton/Program.cs
+++ Source/MeadowSamples/Projects/ServoButton/Program.cs
@@ -9,9 +9,8 @@ namespace ServoButton
         public static void Main(string[] args)
         {
             if (args.Length > 0 && args[0] == "--exitOnDebug") return;
-
-            // instantiate and run new meadow app
-            app = new MeadowApp();            
+            app = new MeadowApp();
+            Thread.Sleep(Timeout.Infinite);
         }
     }
 }
