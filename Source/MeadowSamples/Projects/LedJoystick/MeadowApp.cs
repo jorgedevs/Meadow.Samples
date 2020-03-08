c37dc9aa8861df376f893ada3b17ecccec8fc3ec
Jorge Ramirez <ramirez.jorgea@outlook.com>
2020-03-07 23:24:40 -0800

    [LedJoystick] Update

--- Source/MeadowSamples/Projects/LedJoystick/MeadowApp.cs
+++ Source/MeadowSamples/Projects/LedJoystick/MeadowApp.cs
@@ -1,6 +1,7 @@
 ﻿using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Hid;
 using Meadow.Hardware;
 using System;
 using System.Threading;
@@ -10,60 +11,111 @@ namespace LedJoystick
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
-        //PwmLed up;
-        PwmLed left;
-        PwmLed right;
-        //PwmLed down;
-
-        IAnalogInputPort analogX;
-        IAnalogInputPort analogY;
+        PwmLed Up, Down, Left, Right;
+        AnalogJoystick joystick;
 
         public MeadowApp()
         {
-            Console.WriteLine($"MeadowApp...");
+            Console.WriteLine("Initializing...");
 
-            //up = new PwmLed(Device.CreatePwmPort(Device.Pins.D07), TypicalForwardVoltage.Red);
-            left = new PwmLed(Device.CreatePwmPort(Device.Pins.D02), TypicalForwardVoltage.Red);
-            right = new PwmLed(Device.CreatePwmPort(Device.Pins.D03), TypicalForwardVoltage.Red);
-            //down = new PwmLed(Device.CreatePwmPort(Device.Pins.D04), TypicalForwardVoltage.Red);
+            Up = new PwmLed(Device.CreatePwmPort(Device.Pins.D07, 100, 0.0f), TypicalForwardVoltage.Red);
+            Down = new PwmLed(Device.CreatePwmPort(Device.Pins.D04, 100, 0.0f), TypicalForwardVoltage.Red);
+            Left = new PwmLed(Device.CreatePwmPort(Device.Pins.D02, 100, 0.0f), TypicalForwardVoltage.Red);
+            Right = new PwmLed(Device.CreatePwmPort(Device.Pins.D03, 100, 0.0f), TypicalForwardVoltage.Red);
 
-            analogX = Device.CreateAnalogInputPort(Device.Pins.A01);
-            analogY = Device.CreateAnalogInputPort(Device.Pins.A00);
+            joystick = new AnalogJoystick(
+                Device.CreateAnalogInputPort(Device.Pins.A01),
+                Device.CreateAnalogInputPort(Device.Pins.A00),
+                null, false);
 
-            PulseRgbPwmLed();
+            joystick.SetCenterPosition(); //fire and forget 
+            joystick.Updated += JoystickUpdated;
+            joystick.StartUpdating();
+
+            //var t = TestAnalogJoystick();
         }
 
-        async Task PulseRgbPwmLed()
+        private void JoystickUpdated(object sender, Meadow.Peripherals.Sensors.Hid.JoystickPositionChangeResult e)
         {
-            Console.WriteLine($"PulseRgbPwmLed...");
+            if (e.New.HorizontalValue < 0.2f)
+            {
+                Left.SetBrightness(0f);
+                Right.SetBrightness(0f);
+            }
+            if (e.New.VerticalValue < 0.2f)
+            {
+                Up.SetBrightness(0f);
+                Down.SetBrightness(0f);
+            }
+
+            if (e.New.HorizontalValue > 0)
+                Left.SetBrightness(Math.Abs(e.New.HorizontalValue));
+            else
+                Right.SetBrightness(Math.Abs(e.New.HorizontalValue));
+
+            if (e.New.VerticalValue > 0)
+                Down.SetBrightness(Math.Abs(e.New.VerticalValue));
+            else
+                Up.SetBrightness(Math.Abs(e.New.VerticalValue));
+
+            Console.WriteLine($"({e.New.HorizontalValue}, {e.New.VerticalValue})");
+        }
+
+        async Task TestAnalogJoystick()
+        {
+            Console.WriteLine("TestAnalogJoystick()...");
+
+            float x, y;
 
             while (true)
             {
-                //up.SetBrightness(0);
-                //down.SetBrightness(0);
-                //left.SetBrightness(0);
-                //right.SetBrightness(0);
+                //x = await joystick.GetHorizontalValue();
+                //y = await joystick.GetVerticalValue();
+                //Console.WriteLine($"({x},{y})");
 
-                float x = await analogX.Read(1);
-                float y = await analogY.Read(1);
 
-                float brightnessLeftX = x / 1.58f;
+                var position = await joystick.GetPosition();
+                switch (position)
+                {
+                    case DigitalJoystickPosition.Up:
+                        Down.IsOn = Left.IsOn = Right.IsOn = false;
+                        Up.IsOn = true;
+                        break;
+                    case DigitalJoystickPosition.Down:
+                        Up.IsOn = Left.IsOn = Right.IsOn = false;
+                        Down.IsOn = true;
+                        break;
+                    case DigitalJoystickPosition.Left:
+                        Up.IsOn = Down.IsOn = Right.IsOn = false;
+                        Left.IsOn = true;
+                        break;
+                    case DigitalJoystickPosition.Right:
+                        Up.IsOn = Down.IsOn = Left.IsOn = false;
+                        Right.IsOn = true;
+                        break;
+                    case DigitalJoystickPosition.Center:
+                        Up.IsOn = Down.IsOn = Left.IsOn = Right.IsOn = false;
+                        break;
+                    case DigitalJoystickPosition.UpLeft:
+                        Down.IsOn = Right.IsOn = false;
+                        Up.IsOn = Left.IsOn = true;
+                        break;
+                    case DigitalJoystickPosition.UpRight:
+                        Down.IsOn = Left.IsOn = false;
+                        Up.IsOn = Right.IsOn = true;
+                        break;
+                    case DigitalJoystickPosition.DownLeft:
+                        Up.IsOn = Right.IsOn = false;
+                        Down.IsOn = Left.IsOn = true;
+                        break;
+                    case DigitalJoystickPosition.DownRight:
+                        Up.IsOn = Left.IsOn = false;
+                        Down.IsOn = Right.IsOn = true;
+                        break;
+                }
+                Console.WriteLine($"{position.ToString()}");
 
-                if (brightnessLeftX > 1f)
-                    brightnessLeftX = 1f;
-                left.SetBrightness(1f - brightnessLeftX);
-
-                //if (x < 1.57)
-                //    left.SetBrightness(1);
-                //else if (x > 1.59)
-                //    right.SetBrightness(1);
-
-                //if (y < 1.57)
-                //    up.SetBrightness(1);
-                //else if (y > 1.59)
-                //    down.SetBrightness(1);
-
-                Console.WriteLine($"X: {x}");
+                Thread.Sleep(50);
             }
         }
     }
