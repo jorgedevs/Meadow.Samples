6c23f0f12b37bdb64791b84548f7c9bc9137f6f1
Jorge Ramirez <ramirez.jorgea@outlook.com>
2020-03-28 00:44:22 -0700

    [CarRemote] Update app launcher icons

--- Source/MeadowSamples/ConnectedCar/CarRemote/CarRemote.Android/MainActivity.cs
+++ Source/MeadowSamples/ConnectedCar/CarRemote/CarRemote.Android/MainActivity.cs
@@ -1,15 +1,11 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Runtime;
 
 namespace CarRemote.Droid
 {
-    [Activity(Label = "CarRemote", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CarRemote", Icon = "@mipmap/icon_launcher", RoundIcon = "@mipmap/icon_round_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- Source/MeadowSamples/ConnectedCar/CarRemote/CarRemote.Android/Resources/Resource.designer.cs
+++ Source/MeadowSamples/ConnectedCar/CarRemote/CarRemote.Android/Resources/Resource.designer.cs
@@ -9326,13 +9326,10 @@ namespace CarRemote.Droid
 		{
 			
 			// aapt resource value: 0x7F0C0000
-			public const int icon = 2131492864;
+			public const int icon_launcher = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int icon_round = 2131492865;
-			
-			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			public const int icon_round_launcher = 2131492865;
 			
 			static Mipmap()
 			{
