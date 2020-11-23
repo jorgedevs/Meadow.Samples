a3ced33d44de5196a3ec1ede9e0a6e7b579e798f
Jorge Ramirez <ramirez.jorgea@outlook.com>
2020-11-22 23:11:19 -0800

    Add WifiWeather project

---
+++ Source/MeadowSamples/WifiWeather/MeadowApp.cs
@@ -0,0 +1,46 @@
+﻿using Meadow;
+using Meadow.Devices;
+using Meadow.Foundation;
+using Meadow.Foundation.Leds;
+using Meadow.Gateway.WiFi;
+using System;
+using System.Threading;
+using WifiWeather.ServiceAccessLayer;
+
+namespace WifiWeather
+{
+    public class MeadowApp : App<F7Micro, MeadowApp>
+    {        
+        public MeadowApp()
+        {
+            Initialize();
+
+            ClientServiceFacade.FetchReadings().Wait();
+
+            Console.WriteLine("Done!");
+        }
+
+        void Initialize()
+        {
+            var onboardLed = new RgbPwmLed(device: Device,
+                redPwmPin: Device.Pins.OnboardLedRed,
+                greenPwmPin: Device.Pins.OnboardLedGreen,
+                bluePwmPin: Device.Pins.OnboardLedBlue,
+                3.3f, 3.3f, 3.3f,
+                Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode);
+            onboardLed.SetColor(Color.Red);
+
+            Device.InitWiFiAdapter().Wait();
+
+            onboardLed.SetColor(Color.Blue);
+
+            var result = Device.WiFiAdapter.Connect(Secrets.WIFI_NAME, Secrets.WIFI_PASSWORD);
+            if (result.ConnectionStatus != ConnectionStatus.Success)
+            {
+                throw new Exception($"Cannot connect to network: {result.ConnectionStatus}");
+            }
+
+            onboardLed.SetColor(Color.Green);
+        }
+    }
+}
---
+++ Source/MeadowSamples/WifiWeather/ServiceAccessLayer/ClientServiceFacade.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WifiWeather.Models;
+
+namespace WifiWeather.ServiceAccessLayer
+{
+    public static class ClientServiceFacade
+    {
+        // TODO: change this IP for your localhost 
+        static string climateDataUri = "http://api.openweathermap.org/data/2.5/weather?q=Vancouver&appid=65ffb400cbae35050dc2d1d1ebd3ef8d";
+
+        static ClientServiceFacade() { }
+
+        /// <summary>
+        /// Fetches the climate readings from the Web API Endpoint
+        /// </summary>
+        /// <returns></returns>
+        public static async Task FetchReadings()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.Timeout = new TimeSpan(0, 5, 0);
+
+                HttpResponseMessage response = await client.GetAsync(climateDataUri);
+
+                try
+                {
+                    Console.WriteLine("sending request...");
+
+                    response.EnsureSuccessStatusCode();
+
+                    string json = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(json);
+
+                    var stuff = System.Text.Json.JsonSerializer.Deserialize(json, typeof(WeatherReading));
+
+                    Console.WriteLine("deserialized to object");
+
+                    var reading = stuff as WeatherReading;
+
+                    Console.WriteLine($"Success");
+                }
+                catch (TaskCanceledException)
+                {
+                    Console.WriteLine("Request time out.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Request went sideways: {e.Message}");
+                }
+            }
+        }
+    }
+}
