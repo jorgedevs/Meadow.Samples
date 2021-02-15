d8b14a000cd00153797481903cc433b7f84c9b76
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-02-15 11:51:04 -0800

    Update on I2c-Spi-Analog test

--- Source/MeadowSamples/Tests.I2cSpiAnalogTemperature_Sample/MeadowApp.cs
+++ Source/MeadowSamples/Tests.I2cSpiAnalogTemperature_Sample/MeadowApp.cs
@@ -6,16 +6,18 @@ using Meadow.Foundation.Displays.Tft;
 using Meadow.Foundation.Graphics;
 using Meadow.Foundation.Leds;
 using Meadow.Foundation.Sensors.Temperature;
+using Meadow.Hardware;
 using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using static Meadow.Foundation.Displays.DisplayBase;
 
 namespace Tests.I2cSpiAnalogTemperature_Sample
 {
     public class MeadowApp : App<F7Micro, MeadowApp>
     {
-        Ili9163 displaySPI;
+        St7789 displaySPI;
         GraphicsLibrary graphicsSPI;
         Ssd1306 displayI2C;
         GraphicsLibrary graphicsI2C;
@@ -35,32 +37,38 @@ namespace Tests.I2cSpiAnalogTemperature_Sample
             graphicsI2C.Clear();
             graphicsI2C.Stroke = 1;
             graphicsI2C.DrawRectangle(0, 0, 128, 32);
-            graphicsI2C.DrawText(5, 12, "I2C WORKING");
+            graphicsI2C.DrawText(16, 12, "I2C WORKING!");
             graphicsI2C.Show();
             Console.WriteLine("done");
 
             Console.Write("Initializing SPI...");
-            displaySPI = new Ili9163(
+            var config = new SpiClockConfiguration(24000, SpiClockConfiguration.Mode.Mode3);
+            var spiBus = Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, config);
+
+            displaySPI = new St7789
+            (
                 device: Device,
-                spiBus: Device.CreateSpiBus(),
-                chipSelectPin: Device.Pins.D14,
-                dcPin: Device.Pins.D11,
-                resetPin: Device.Pins.D10,
-                width: 128, height: 160);
+                spiBus: spiBus,
+                chipSelectPin: null,
+                dcPin: Device.Pins.D14,
+                resetPin: Device.Pins.D13,
+                width: 240, height: 240,
+                displayColorMode: DisplayColorMode.Format16bppRgb565
+            );
             graphicsSPI = new GraphicsLibrary(displaySPI);
             graphicsSPI.Rotation = GraphicsLibrary.RotationType._90Degrees;
             graphicsSPI.Clear();
-            graphicsSPI.Stroke = 1;
-            graphicsSPI.DrawRectangle(0, 0, 160, 128);
+            graphicsSPI.Stroke = 3;
+            graphicsSPI.DrawRectangle(0, 0, 240, 240);
             graphicsSPI.CurrentFont = new Font8x12();
-            graphicsSPI.DrawText(7, 7, "SPI WORKING!", Color.White);
+            graphicsSPI.DrawText(24, 12, "SPI WORKING!", Color.White, GraphicsLibrary.ScaleFactor.X2);
             graphicsSPI.Show();
             Console.WriteLine("done");
 
             temperatures = new List<AnalogTemperature>
             {
-                new AnalogTemperature(Device, Device.Pins.A00, AnalogTemperature.KnownSensorType.LM35),
-                new AnalogTemperature(Device, Device.Pins.A01, AnalogTemperature.KnownSensorType.LM35),
+                //new AnalogTemperature(Device, Device.Pins.A00, AnalogTemperature.KnownSensorType.LM35),
+                //new AnalogTemperature(Device, Device.Pins.A01, AnalogTemperature.KnownSensorType.LM35),
                 new AnalogTemperature(Device, Device.Pins.A02, AnalogTemperature.KnownSensorType.LM35),
                 new AnalogTemperature(Device, Device.Pins.A03, AnalogTemperature.KnownSensorType.LM35),
                 new AnalogTemperature(Device, Device.Pins.A04, AnalogTemperature.KnownSensorType.LM35), 
@@ -94,13 +102,13 @@ namespace Tests.I2cSpiAnalogTemperature_Sample
 
                     if (temp == null)
                     {
-                        graphicsSPI.DrawRectangle(7, i * 17 + 25, 130, 12, Color.Black, true);
-                        graphicsSPI.DrawText(7, i * 17 + 25, $"A0{i}: INACTIVE", Color.White);
+                        graphicsSPI.DrawRectangle(7, i * 32 + 44, 208, 24, Color.Black, true);
+                        graphicsSPI.DrawText(7, i * 32 + 44, $"A0{i}: INACTIVE", Color.Red, GraphicsLibrary.ScaleFactor.X2);
                     }
                     else
                     {
-                        graphicsSPI.DrawRectangle(7, i * 17 + 25, 130, 12, Color.Black, true);
-                        graphicsSPI.DrawText(7, i * 17 + 25, $"{temperatures[tempIndex].AnalogInputPort.Pin}: {temp}", Color.White);
+                        graphicsSPI.DrawRectangle(7, i * 32 + 44, 208, 24, Color.Black, true);
+                        graphicsSPI.DrawText(7, i * 32 + 44, $"{temperatures[tempIndex].AnalogInputPort.Pin}: {temp}", Color.Cyan, GraphicsLibrary.ScaleFactor.X2);
                         tempIndex++;
                     }
 
