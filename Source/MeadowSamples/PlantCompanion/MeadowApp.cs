05416a6d5eaef69b22cfed7b46b4c09d5c096c3e
Jorge Ramirez <ramirez.jorgea@outlook.com>
2021-02-20 23:36:00 -0800

    PlantCompanion updates

--- Source/MeadowSamples/PlantCompanion/MeadowApp.cs
+++ Source/MeadowSamples/PlantCompanion/MeadowApp.cs
@@ -4,9 +4,14 @@ using Meadow.Foundation;
 using Meadow.Foundation.Displays.Tft;
 using Meadow.Foundation.Graphics;
 using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Buttons;
 using Meadow.Foundation.Sensors.Moisture;
+using Meadow.Foundation.Sensors.Temperature;
 using Meadow.Hardware;
+using SimpleJpegDecoder;
 using System;
+using System.IO;
+using System.Reflection;
 using System.Threading;
 
 namespace PlantCompanion
@@ -17,9 +22,20 @@ namespace PlantCompanion
         const float MAXIMUM_VOLTAGE_CALIBRATION = 1.50f;
 
         RgbPwmLed onboardLed;
-        Capacitive capacitive;
-        St7789 display;
+        PushButton button;
+        Capacitive capacitive;        
         GraphicsLibrary graphics;
+        AnalogTemperature analogTemperature;
+
+        string[] images = new string[4] 
+        { 
+            "level_0.jpg", 
+            "level_1.jpg", 
+            "level_2.jpg",
+            "level_3.jpg"
+        };
+
+        int selectedImageIndex = 0;
 
         public MeadowApp()
         {
@@ -40,12 +56,15 @@ namespace PlantCompanion
                 Meadow.Peripherals.Leds.IRgbLed.CommonType.CommonAnode);
             onboardLed.SetColor(Color.Red);
 
+            button = new PushButton(Device, Device.Pins.D04, ResistorMode.InternalPullUp);
+            button.Clicked += ButtonClicked;
+
             var config = new SpiClockConfiguration
             (
                 speedKHz: 6000,
                 mode: SpiClockConfiguration.Mode.Mode3
             );
-            display = new St7789
+            var display = new St7789
             (
                 device: Device,
                 spiBus: Device.CreateSpiBus(Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, config),
@@ -56,15 +75,54 @@ namespace PlantCompanion
             );
 
             graphics = new GraphicsLibrary(display);
-            graphics.Rotation = GraphicsLibrary.RotationType._270Degrees;
+
+
 
             capacitive = new Capacitive(
                 device: Device, 
-                analogPin: Device.Pins.A00, 
+                analogPin: Device.Pins.A01, 
                 minimumVoltageCalibration: MINIMUM_VOLTAGE_CALIBRATION,
                 maximumVoltageCalibration: MAXIMUM_VOLTAGE_CALIBRATION);
 
+            analogTemperature = new AnalogTemperature(Device, Device.Pins.A00, AnalogTemperature.KnownSensorType.LM35);
+            
 
+
+            onboardLed.SetColor(Color.Green);
+        }
+
+        async void ButtonClicked(object sender, EventArgs e)
+        {
+            onboardLed.SetColor(Color.Orange);
+
+            float moisture = await capacitive.Read();
+
+            if (moisture > 1)
+                moisture = 1f;
+            else
+            if (moisture < 0)
+                moisture = 0f;
+
+
+            if (moisture > 0 && moisture <= 0.25)
+                selectedImageIndex = 0;
+            else if (moisture > 0.25 && moisture <= 0.50)
+                selectedImageIndex = 1;
+            else if (moisture > 0.50 && moisture <= 0.75)
+                selectedImageIndex = 2;
+            else if (moisture > 0.75 && moisture <= 1.0)
+                selectedImageIndex = 3;
+
+            DisplayJPG();
+
+            int percentage = (int)(moisture * 100);
+
+            graphics.DrawRectangle(0, 0, 36, 20, Color.White, true);
+            graphics.DrawText(0, 0, $"{(int)(moisture * 100)}%", Color.Black);            
+
+
+            graphics.Show();
+            
             onboardLed.SetColor(Color.Green);
         }
 
@@ -76,9 +134,57 @@ namespace PlantCompanion
             graphics.DrawRectangle(0, 0, 240, 240, Color.White, true);
 
             graphics.CurrentFont = new Font12x20();
-            graphics.DrawText(120, 120, "99%", Color.Black, GraphicsLibrary.ScaleFactor.X2);
+            graphics.DrawText(0, 0, "99%", Color.Black);
+
+            DisplayJPG();
 
             graphics.Show();
+        }
+
+        void DisplayJPG()
+        {
+            var jpgData = LoadResource(images[selectedImageIndex]);
+            var decoder = new JpegDecoder();
+            var jpg = decoder.DecodeJpeg(jpgData);
+
+            int x = 0;
+            int y = 0;
+            byte r, g, b;
+
+            graphics.DrawRectangle(25, 25, 190, 190, Color.White, true);
+
+            for (int i = 0; i < jpg.Length; i += 3)
+            {
+                r = jpg[i];
+                g = jpg[i + 1];
+                b = jpg[i + 2];
+
+                graphics.DrawPixel(x + 25, y + 25, Color.FromRgb(r, g, b));
+
+                x++;
+                if (x % decoder.Width == 0)
+                {
+                    y++;
+                    x = 0;
+                }
+            }
+
+           graphics.Show();
+        }
+
+        byte[] LoadResource(string filename)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = $"PlantCompanion.{filename}";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
         }
     }
 }
