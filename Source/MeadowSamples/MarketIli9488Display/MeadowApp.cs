852ba1340944c3df5a6a56121eecee0d4d0a2d70
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-03-26 11:16:38 -0700

    Update MarketIli9488Display

--- Source/MeadowSamples/MarketIli9488Display/MeadowApp.cs
+++ Source/MeadowSamples/MarketIli9488Display/MeadowApp.cs
@@ -1,9 +1,16 @@
 ﻿using Meadow;
 using Meadow.Devices;
 using Meadow.Foundation;
+using Meadow.Foundation.Displays;
+using Meadow.Foundation.Graphics;
 using Meadow.Foundation.Leds;
+using Meadow.Hardware;
 using Meadow.Peripherals.Leds;
+using Meadow.Units;
+using SimpleJpegDecoder;
 using System;
+using System.IO;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -13,14 +20,7 @@ namespace MarketIli9488Display
     public class MeadowApp : App<F7FeatherV2>
     {
         RgbPwmLed onboardLed;
-
-        public override Task Run()
-        {
-            Console.WriteLine("Run...");
-
-            CycleColors(TimeSpan.FromMilliseconds(1000));
-            return base.Run();
-        }
+        MicroGraphics graphics;
 
         public override Task Initialize()
         {
@@ -31,36 +31,100 @@ namespace MarketIli9488Display
                 greenPwmPin: Device.Pins.OnboardLedGreen,
                 bluePwmPin: Device.Pins.OnboardLedBlue,
                 CommonType.CommonAnode);
+            onboardLed.SetColor(Color.Red);
 
+            var config = new SpiClockConfiguration(
+                new Frequency(12000, Frequency.UnitType.Kilohertz),
+                SpiClockConfiguration.Mode.Mode0);
+            var spiBus = Device.CreateSpiBus(
+                Device.Pins.SCK, Device.Pins.MOSI, Device.Pins.MISO, config);
+
+            var display = new Ili9488
+            (
+                spiBus: spiBus,
+                chipSelectPin: Device.Pins.D02,
+                dcPin: Device.Pins.D00,
+                resetPin: Device.Pins.D01,
+                width: 480,
+                height: 320
+            );
+
+            graphics = new MicroGraphics(display)
+            {
+                IgnoreOutOfBoundsPixels = true,
+                CurrentFont = new Font8x8(), 
+                Rotation = RotationType._270Degrees
+            };
+
+            graphics.Clear();
+            DisplayJPG(0, 0, "img_devcamp.jpg");
+            graphics.Show();
+
+            onboardLed.SetColor(Color.Green);
             return base.Initialize();
         }
 
-        void CycleColors(TimeSpan duration)
+        void DisplayJPG(int x, int y, string filename)
         {
-            Console.WriteLine("Cycle colors...");
+            var jpgData = LoadResource(filename);
+            var decoder = new JpegDecoder();
+            var jpg = decoder.DecodeJpeg(jpgData);
 
-            while (true)
+            int x_offset = 0;
+            int y_offset = 0;
+            byte r, g, b;
+
+            for (int i = 0; i < jpg.Length; i += 3)
             {
-                ShowColorPulse(Color.Blue, duration);
-                ShowColorPulse(Color.Cyan, duration);
-                ShowColorPulse(Color.Green, duration);
-                ShowColorPulse(Color.GreenYellow, duration);
-                ShowColorPulse(Color.Yellow, duration);
-                ShowColorPulse(Color.Orange, duration);
-                ShowColorPulse(Color.OrangeRed, duration);
-                ShowColorPulse(Color.Red, duration);
-                ShowColorPulse(Color.MediumVioletRed, duration);
-                ShowColorPulse(Color.Purple, duration);
-                ShowColorPulse(Color.Magenta, duration);
-                ShowColorPulse(Color.Pink, duration);
+                r = jpg[i];
+                g = jpg[i + 1];
+                b = jpg[i + 2];
+
+                graphics.DrawPixel(x + x_offset, y + y_offset, Color.FromRgb(r, g, b));
+
+                x_offset++;
+                if (x_offset % decoder.Width == 0)
+                {
+                    y_offset++;
+                    x_offset = 0;
+                }
+            }
+
+            graphics.Show();
+        }
+
+        byte[] LoadResource(string filename)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = $"MarketIli9488Display.{filename}";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
             }
         }
 
-        void ShowColorPulse(Color color, TimeSpan duration)
+
+        public override async Task Run()
         {
-            onboardLed.StartPulse(color, duration / 2);
-            Thread.Sleep(duration);
-            onboardLed.Stop();
+            Console.WriteLine("Run...");
+
+            while (true) 
+            {
+                DisplayJPG(0, 0, "img_devcamp.jpg");
+                graphics.Show();
+
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                DisplayJPG(0, 0, "img_devcamp_qr.jpg");
+                graphics.Show();
+
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
         }
     }
 }
