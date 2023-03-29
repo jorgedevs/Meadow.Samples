c15e56079ad883fad13d4e873d811fdf2ba6aa0c
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-03-29 01:13:23 -0700

    Update MarketEpaperDisplay

--- Source/MeadowSamples/MarketEpaperDisplay/MeadowApp.cs
+++ Source/MeadowSamples/MarketEpaperDisplay/MeadowApp.cs
@@ -5,8 +5,10 @@ using Meadow.Foundation.Displays;
 using Meadow.Foundation.Graphics;
 using Meadow.Foundation.Leds;
 using Meadow.Peripherals.Leds;
+using SimpleJpegDecoder;
 using System;
-using System.Threading;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace MarketEpaperDisplay
@@ -27,28 +29,71 @@ namespace MarketEpaperDisplay
                 CommonType.CommonAnode);
             onboardLed.SetColor(Color.Red);
 
-            var display = new Ssd1680(
+            var display = new Epd4in2bV2(
                 spiBus: Device.CreateSpiBus(),
-                chipSelectPin: Device.Pins.A05,
-                dcPin: Device.Pins.A04,
+                chipSelectPin: Device.Pins.A04,
+                dcPin: Device.Pins.A03,
                 resetPin: Device.Pins.A02,
-                busyPin: Device.Pins.A01,
-                width: 122,
-                height: 250);
+                busyPin: Device.Pins.A01);
 
             graphics = new MicroGraphics(display)
             {
-                Rotation = RotationType._270Degrees,
-                CurrentFont = new Font12x16()
+                CurrentFont = new Font12x16(),
+                Stroke = 2,
+                Rotation = RotationType._180Degrees
             };
 
             graphics.Clear();
-            graphics.DrawText(10, 10, "Hello World");
+            DisplayJPG(0, 0, "img_devcamp.jpg");
             graphics.Show();
 
             onboardLed.SetColor(Color.Green);
 
             return base.Initialize();
+        }
+
+        void DisplayJPG(int x, int y, string filename)
+        {
+            var jpgData = LoadResource(filename);
+            var decoder = new JpegDecoder();
+            var jpg = decoder.DecodeJpeg(jpgData);
+
+            int x_offset = 0;
+            int y_offset = 0;
+            byte r, g, b;
+
+            for (int i = 0; i < jpg.Length; i += 3)
+            {
+                r = jpg[i];
+                g = jpg[i + 1];
+                b = jpg[i + 2];
+
+                graphics.DrawPixel(x + x_offset, y + y_offset, Color.FromRgb(r, g, b).Brightness < 0.85);
+
+                x_offset++;
+                if (x_offset % decoder.Width == 0)
+                {
+                    y_offset++;
+                    x_offset = 0;
+                }
+            }
+
+            graphics.Show();
+        }
+
+        byte[] LoadResource(string filename)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = $"MarketEpaperDisplay.{filename}";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
         }
 
         public override Task Run()
