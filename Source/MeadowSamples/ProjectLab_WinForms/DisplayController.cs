0983f6a56aa9e5a8d939230be61e4da5ff19038c
Jorge Ramirez <ramirez.jorgea@outlook.com>
2023-10-02 00:58:26 -0700

    Add ProjectLab_WinForms

---
+++ Source/MeadowSamples/ProjectLab_WinForms/DisplayController.cs
@@ -0,0 +1,29 @@
+﻿using Meadow.Foundation.Graphics;
+using Meadow.Foundation.Graphics.MicroLayout;
+using Meadow.Units;
+
+namespace ProjectLab_WinForms
+{
+    public class DisplayController
+    {
+        DisplayScreen _screen;
+
+        public DisplayController(IGraphicsDisplay display)
+        {
+            _screen = new DisplayScreen(display);
+            _screen.Controls.Add(
+            new Box(0, 0, _screen.Width / 2, _screen.Height / 2)
+            {
+                    ForeColor = Meadow.Foundation.Color.Red
+            },
+            new Meadow.Foundation.Graphics.MicroLayout.Label(0, 0, _screen.Width / 2, _screen.Height / 2)
+            {
+                    Text = "Hello World!",
+                    TextColor = Meadow.Foundation.Color.Black,
+                    BackColor = Meadow.Foundation.Color.Transparent,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = Meadow.Foundation.Graphics.HorizontalAlignment.Center
+                });
+        }
+    }
+}
---
+++ Source/MeadowSamples/ProjectLab_WinForms/MeadowApp.cs
@@ -0,0 +1,40 @@
+﻿using Meadow;
+using Meadow.Foundation.Displays;
+
+namespace ProjectLab_WinForms
+{
+    public class MeadowApp : App<Windows>
+    {
+        private WinFormsDisplay _display = default!;
+        private DisplayController _displayController;
+
+        public override Task Initialize()
+        {
+            Console.WriteLine("Initialize...");
+
+            _display = new WinFormsDisplay(width: 320, height: 240);
+            _displayController = new DisplayController(_display);
+
+            return Task.CompletedTask;
+        }
+
+        public override Task Run()
+        {
+            Console.WriteLine("Run...");
+
+            Application.Run(_display);
+
+            return Task.CompletedTask;
+        }
+
+        public static async Task Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
+            ApplicationConfiguration.Initialize();
+
+            await MeadowOS.Start(args);
+        }
+    }
+}
